c86d3e3979f909351fa759991d78d7874f6a8fd7
wizard <wizard@mail.com>
2025-07-05 22:11:19 +0300

    add example

---
+++ example.py
@@ -0,0 +1,23 @@
+# Полиморфизм и doctyping
+
+
+class WildDuck:
+    def quack(slrf) -> str:
+        return "Quack!"
+
+    def some_decoy_method(self):
+        pass
+
+
+
+class Decoy:
+    def quack(self) -> str:
+        return "*False quack"
+    
+    def some_decoy_method(self):
+        pass
+
+
+
+def actions(value):
+    print(value.quack())
